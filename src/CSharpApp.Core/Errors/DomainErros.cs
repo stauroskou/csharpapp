1d8ea94521970b7c085210afe0b5052303e98090
Stavros Koutsoukos <stauroskoutsoukos@gmail.com>
2025-01-22 00:06:21 +0200

    feature(result object): created result object

---
+++ src/CSharpApp.Core/Errors/DomainErros.cs
@@ -0,0 +1,20 @@
+﻿using CSharpApp.Core.Shared;
+
+namespace CSharpApp.Core.Errors;
+
+public static class DomainErros
+{
+    public static class Products
+    {
+        public static readonly Error CreationFailed = new(
+            "Products.CreationFailed",
+            "Creation of the product failed");
+        public static readonly Error EmptyTitle = new(
+            "Products.EmptyTitle",
+            "title should not be empty");
+
+        public static readonly Error EmptyDescription = new(
+            "Products.EmptyTitle",
+            "title should not be empty");
+    }
+}
